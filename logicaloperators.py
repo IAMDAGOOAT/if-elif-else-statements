7a0c99b1e7a8c09bfe9e6e107c4b1942b7280a39
IAMDAGOOAT <gopinathanaar@gmail.com>
2025-07-15 12:43:47 +0800

    Logical Operators

---
+++ logicaloperators.py
@@ -0,0 +1,12 @@
+a = 10
+b = 10
+
+if a > 0 and b > 0:
+    print("the number is greater than 0")
+
+p = 10
+q = -10
+if p > 0 or q > 0:
+    print("either of the number is greater than 0")
+else:
+    print("neither of the number is greater than 0")
