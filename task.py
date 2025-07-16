245b112cd7cbca9897868d272a88e4b52b7a536c
IAMDAGOOAT <gopinathanaar@gmail.com>
2025-07-16 12:23:10 +0800

    Sercret Organization

---
+++ task.py
@@ -0,0 +1,14 @@
+password=input("enter the Password: ")
+password=int(password)
+
+if password==1999:
+    print("there is a traintor in team B. Find him without lettingthe team know cause it can be anyone")
+    
+elif password==2422:
+   print("the president is in danger. Keep a watch on him secretly")
+elif password==1832:
+   print("there is a traitor in Team A. He is off guard for know because of wrong info.")
+elif password==3943:
+   print("assasinate the minister")
+else:
+    print("Intruder Alert")
